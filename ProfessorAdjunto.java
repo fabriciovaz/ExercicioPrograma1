883009ac0f1925738d4701c16573c0e15714dc77
Fabrício Fernandes Vaz <40251102+fabriciovaz@users.noreply.github.com>
2018-06-18 00:40:36 -0300

    Delete ProfessorAdjunto.java

--- ProfessorAdjunto.java
+++
@@ -1,8 +0,0 @@
-public class ProfessorAdjunto extends Professor { //Herda os atributos da classe professor e acrescenta quantidade de horas de monitoria
-   int quantHorasMonitoria;
-
-    public ProfessorAdjunto(String nome, String sobrenome, int tempoDeCasa, int id, int quantHorasMonitoria) {
-        super(nome, sobrenome, tempoDeCasa, id);
-        this.quantHorasMonitoria = quantHorasMonitoria;
-    }
-}
